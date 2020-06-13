c8c29cedaf99ce8819f7f5a3bb084b0639bfa5ea
higashi000 <higashi.ict@gmail.com>
2020-06-14 00:43:46 +0900

    Add ws server

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/higashi000/noa
 
 go 1.14
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gin-gonic/gin v1.6.3
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/valyala/fasttemplate v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20200604202706-70a84ac30bf9 // indirect
+	gopkg.in/olahol/melody.v1 v1.0.0-20170518105555-d52139073376
+)
--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"gopkg.in/olahol/melody.v1"
+)
+
+type Msg struct {
+	Text string `json:"text"`
+	Name string `json:"name"`
+}
 
 func main() {
-	fmt.Println("noa")
+	r := gin.Default()
+	m := melody.New()
+
+	r.GET("/ws", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+
+	r.POST("/send", func(c *gin.Context) {
+		var tmp Msg
+		c.BindJSON(&tmp)
+
+		m.Broadcast([]byte(tmp.Name + ": " + tmp.Text))
+	})
+
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		m.Broadcast(msg)
+	})
+
+	r.Run(":5000")
 }
