bdfc5e6851575a93d4b4388cde576387517d822b
higashi000 <higashi.ict@gmail.com>
2020-06-19 23:59:08 +0900

    Add regist channel

---
+++ registchannel/registchannel.go
@@ -0,0 +1,51 @@
+package registchannel
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Channel struct {
+	RoomId   string `json:"roomid" bson:"roomid"`
+	Password string `json:"password" bson:"password"`
+	Admin    string `json:"admin" bson:"admin"`
+}
+
+func RegistChannel(r *gin.Engine, db *mongo.Database) {
+	channelColl := db.Collection("channel")
+
+	findOptions := options.Find()
+	cur, err := channelColl.Find(context.TODO(), bson.D{{}}, findOptions)
+	if err != nil {
+		log.Println(err)
+	}
+
+	r.POST("noa/registchannel", func(c *gin.Context) {
+		var recvChannelData Channel
+		c.BindJSON(&recvChannelData)
+
+		for cur.Next(context.Background()) {
+			var tmp Channel
+
+			cur.Decode(&tmp)
+
+			if recvChannelData.RoomId == tmp.RoomId {
+				c.JSON(http.StatusOK, `{"status": "false", "message":"This ID is already used."}`)
+			}
+		}
+
+		res, err := channelColl.InsertOne(context.Background(), recvChannelData)
+		if err != nil {
+			log.Println(err)
+		}
+
+		c.JSON(http.StatusOK, `{"status": "true", "message":"Correct"}`)
+		log.Println(res)
+	})
+}
