90a7fb01e9fbfb051fb5325dad873be82a00ba31
higashi000 <higashi.ict@gmail.com>
2020-06-20 00:08:05 +0900

    Add mongodb client

--- main.go
+++ main.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"context"
+	"log"
+
 	"github.com/gin-gonic/gin"
 	"github.com/higashi000/noa/initclient"
 	"github.com/higashi000/noa/recvmsg"
+	"github.com/higashi000/noa/registchannel"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"gopkg.in/olahol/melody.v1"
 )
 
 func main() {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://root:example@localhost:27017"))
+	if err != nil {
+		log.Println(err)
+	}
+
+	err = client.Connect(context.Background())
+	if err != nil {
+		log.Println(err)
+	}
+
+	db := client.Database("noa")
+
 	r := gin.Default()
 	m := melody.New()
 
@@ -15,6 +33,7 @@ func main() {
 		m.HandleRequest(c.Writer, c.Request)
 	})
 
+	registchannel.RegistChannel(r, db)
 	recvmsg.RecvMsg(r, m)
 	initclient.InitClient(r)
 
